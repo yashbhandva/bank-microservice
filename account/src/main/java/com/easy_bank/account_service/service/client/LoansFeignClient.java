939ec22e339742a0b2e39e9e3f6467e06e613c63
yashbhandva <yashbhandva01@gmail.com>
2025-12-24 23:57:51 +0530

    Create LoansFeignClient.java

---
+++ account/src/main/java/com/easy_bank/account_service/service/client/LoansFeignClient.java
@@ -0,0 +1,14 @@
+package com.easy_bank.account_service.service.client;
+
+import com.easy_bank.account_service.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "loan-service")
+public interface LoansFeignClient {
+
+    @GetMapping("/api/fetch")
+    ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);
+}
