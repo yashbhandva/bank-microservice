52885a5efa5c5b39eff5d8f73cc97715199d1fde
yashbhandva <yashbhandva01@gmail.com>
2025-12-24 22:20:29 +0530

    Update AccountServiceApplication.java

--- account/src/main/java/com/easy_bank/account_service/AccountServiceApplication.java
+++ account/src/main/java/com/easy_bank/account_service/AccountServiceApplication.java
@@ -2,10 +2,12 @@ package com.easy_bank.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableFeignClients
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
