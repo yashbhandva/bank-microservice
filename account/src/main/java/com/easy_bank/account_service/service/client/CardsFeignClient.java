3de648ba6c66e02e3179c8485fd95cf93a6f2d01
yashbhandva <yashbhandva01@gmail.com>
2025-12-24 23:57:49 +0530

    Update CardsFeignClient.java

--- account/src/main/java/com/easy_bank/account_service/service/client/CardsFeignClient.java
+++ account/src/main/java/com/easy_bank/account_service/service/client/CardsFeignClient.java
@@ -1,7 +1,14 @@
 package com.easy_bank.account_service.service.client;
 
+import com.easy_bank.account_service.dto.CardsDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "card-service")
 public interface CardsFeignClient {
+
+    @GetMapping("/api/fetch")
+    ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);
 }
